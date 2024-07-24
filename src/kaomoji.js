232febd2d39c6c79cdafc3eae7ab6afaefcf896c
Jay Que <54558959+molochxte@users.noreply.github.com>
2024-07-24 16:03:36 -0700

    improved tabs

--- src/kaomoji.js
+++ src/kaomoji.js
@@ -9,6 +9,7 @@ function copyToClipboard() {
 
 function clearKaomoji() {
     document.getElementById("kaomoji_display").value = "";
+    kaomoji = ["","","","","","",""];
 }
 
 window.onload = function() {
@@ -40,7 +41,7 @@ var kaomoji = ["","","","","","",""];
 // create 3x3 buttons in a div with id "buttons" that will change the text in the text area of id="kaomoji" to the corresponding kaomoji
 function createEyeButtons() {
     console.log("Creating buttons...");
-    var buttons = document.getElementById("eye_menu");
+    var buttons = document.getElementById("Eyes");
     for (var i = 0; i < eyes.length; i++) {
         var button = document.createElement("button");
         button.classList.add("border-2",  "rounded-lg", "p-2", "m-2");
@@ -54,37 +55,9 @@ function createEyeButtons() {
     }
 }
 
-function showEyeMenu() {
-    var eye_menu = document.getElementById("eye_menu");
-    eye_menu.classList.toggle("hidden");
-    var tab_eye = document.getElementById("tab_eye");
-    tab_eye.classList.add("border-purple-400")
-    disableMouthMenu();
-}
-
-function showMouthMenu() {
-    var mouth_menu = document.getElementById("mouth_menu");
-    mouth_menu.classList.toggle("hidden");
-    var tab_mouth = document.getElementById("tab_mouth");
-    tab_mouth.classList.add("border-purple-400")
-    disableEyeMenu();
-}
-
-function disableEyeMenu() {
-    var eye_menu = document.getElementById("eye_menu");
-    eye_menu.classList.add("hidden");
-    var tab_eye = document.getElementById("tab_eye");
-    tab_eye.classList.remove("border-purple-400")
-}
-function disableMouthMenu() {
-    var mouth_menu = document.getElementById("mouth_menu");
-    mouth_menu.classList.add("hidden");
-    var tab_mouth = document.getElementById("tab_mouth");
-    tab_mouth.classList.remove("border-purple-400")
-}
 function createMouthButtons() {
     console.log("Creating buttons...");
-    var buttons = document.getElementById("mouth_menu");
+    var buttons = document.getElementById("Mouths");
     for (var i = 0; i < mouth.length; i++) {
         var button = document.createElement("button");
         button.classList.add("border-2", "rounded-lg", "p-2", "m-2");
@@ -97,3 +70,43 @@ function createMouthButtons() {
     }
 }
 
+function switchTab(e){
+
+    tab_name = e.target.innerText;
+    active_tab = e.target;
+    all_tabs = document.getElementsByClassName("tabs");
+    all_options = document.getElementsByClassName("options");
+    active_option = document.getElementById(tab_name);
+    console.log("tab_name", tab_name);
+    console.log("active_option",active_option);
+    console.log("all_options", all_options);
+    console.log("all_tabs", all_tabs);
+    console.log("active_tab", active_tab);
+
+  
+    disableTabs();
+    hideOptions();
+    active_tab.classList.add("text-purple-500");
+    console.log(active_tab.classList);
+    active_option.classList.remove("hidden");
+
+    // active_tab = the button that was clicked
+    // all_tabs = all buttons in the tabs class
+    // all_options = all options (emojis) in the options class
+    // active_option = the option that is currently active
+
+}
+
+function disableTabs(){
+    all_tabs = document.getElementsByClassName("tabs");
+    for (var i = 0; i < all_tabs.length; i++) {
+        all_tabs[i].classList.remove("text-purple-500");
+    }
+}
+
+function hideOptions(){
+    all_options = document.getElementsByClassName("options");
+    for (var i = 0; i < all_options.length; i++) {
+        all_options[i].classList.add("hidden");
+    }
+}
